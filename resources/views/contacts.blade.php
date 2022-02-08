c8d75a4fff7885a94e0b44754476458854b0a346
daffanaufal <daffanaufalathallah@gmail.com>
2022-02-08 12:14:17 +0700

    upload modul 4

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -5,20 +5,21 @@
 <div class="tittle-top mb-5">
     <h2 class="fw-bold">Contacts Us</h2>
 </div>
-<form>
-<div class="form-floating mb-3">
-  <input type="text" class="form-control" id="inputname" placeholder="Nama">
-  <label for="inputname">Nama</label>
-</div>
-<div class="form-floating mb-3">
-  <input type="email" class="form-control" id="inputemail" placeholder="Email">
-  <label for="inputemail">Email</label>
-</div>
-<div class="form-floating mb-3">
-    <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea2" style="height: 100px"></textarea>
-    <label for="floatingTextarea2">Pesan</label>
-</div>
-<button type="sumbit" class="btn btn-primary">sumbit</button>
+<form method="post" action="{{route('contacts.store')}}">
+  {{csrf_field()}}
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
+        <label for="inputname">Nama</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="inputemail" placeholder="Email" name="email">
+        <label for="inputemail">Email</label>
+    </div>
+    <div class="form-floating mb-3">
+        <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea2" style="height: 100px" name="pesan"></textarea>
+        <label for="floatingTextarea2">Pesan</label>
+    </div>
+<button type="sumbit" class="btn btn-primary">Kirim</button>
 </form>
 
 @endsection
