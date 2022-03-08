9090b07c4c1d8112c785a973aac84f8e9e2b10af
Daff-Tech <daffanaufalathallah@gmail.com>
2022-03-08 11:43:34 +0700

    upload modul 7

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <div class="container">
-    <a class="navbar-brand" href="#">Daffa</a>
+    <a class="navbar-brand" href="/">AxxA</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="#">Home</a>
+          <a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="#">About</a>
+          <a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="/about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="#">Gallery</a>
+          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="/gallery">Gallery</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="#">Contact Us</a>
+          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="{{ route('contacts.create')}}">Contact Us</a>
         </li>
       </ul>
       <ul class="navbar-nav ms-auto">
